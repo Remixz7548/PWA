bc554957999f85f1e1a15c9c5205be51a9de9799
Remixz7548 <74849956+Remixz7548@users.noreply.github.com>
2023-11-27 11:31:55 -0600

    Add nav

--- index.js
+++ index.js
@@ -4,3 +4,26 @@ function toggleMenu() {
     menuToggle.classList.toggle("active");
     navbar.classList.toggle("active");
 }
+
+    var prevScrollPos = window.pageYOffset;
+    var threshold = 100; // Establece el umbral según tus necesidades
+    var isAtTop = true;
+
+    window.addEventListener('scroll', function () {
+        var currentScrollPos = window.pageYOffset;
+        var header = document.querySelector('header');
+
+        if (currentScrollPos > prevScrollPos && currentScrollPos > threshold) {
+            // Desplazamiento hacia abajo y supera el umbral
+            header.classList.add('scrolled-down');
+            isAtTop = false;
+        } else {
+            // Desplazamiento hacia arriba o en la parte superior
+            if (currentScrollPos <= threshold && !isAtTop) {
+                header.classList.remove('scrolled-down');
+                isAtTop = true;
+            }
+        }
+
+        prevScrollPos = currentScrollPos;
+    });
